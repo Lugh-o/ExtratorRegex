c52a0d7def6279b2e7c39e45ec74a4d6fd64864e
Lucas Falcão <lughfalcao@gmail.com>
2025-09-16 15:21:51 -0300

    tests: tests created

---
+++ src/main/groovy/com/acelerazg/ExtratorRegex.groovy
@@ -0,0 +1,10 @@
+package com.acelerazg
+
+class ExtratorRegex {
+    static ArrayList<String> encontrarPalavrasComLetrasBr(String input){}
+    static Map<String, ArrayList<String>> encontrarEncontrosSilabicos(String input){}
+    static String limparPlurais(String input){}
+    static ArrayList<String> encontrarProparoxitonas(String input){}
+    static ArrayList<String> encontrarFrasesComQuatroPalavras(String input){}
+    static Map<String, Integer> encontrarFrasesRepetidas(String input){}
+}
